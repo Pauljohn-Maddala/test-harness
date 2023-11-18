731c82380ccf171284b0be6b9c0bdcf626278fd9
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-11-18 16:39:53 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,46 +8,36 @@ class TestProgram(unittest.TestCase):
 
     def run_test(self, program, test_name, use_args=False):
         input_file = f'{self.test_dir}/{program}.{test_name}.in'
-        expected_file = f'{self.test_dir}/{program}.{test_name}' + ('.arg.out' if use_args else '.out')
-    
+        expected_file = f'{self.test_dir}/{program}.{test_name}'+ ('.arg.out' if use_args else '.out')
+
+        # Read arguments from the .in file
+        with open(input_file, 'r') as f:
+            # Split the arguments into a list. Assumes that the input file has the arguments on one line.
+            args = f.read().strip().split(' ', 1)
+
         # Construct the command
-        cmd = ['python3', os.path.join(self.prog_dir, f'{program}.py')]
-    
-        if use_args:
-            # If use_args is True, pass input_file as a command-line argument
-            cmd.append(input_file)
-            process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        else:
-            # If use_args is False and the program is wc, pass the content of input_file as STDIN
-            if program == 'wc':
-                with open(input_file, 'r') as f:
-                    input_content = f.read()
-                process = subprocess.run(cmd, input=input_content.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            else:
-                # For other programs, pass the filename as a command-line argument
-                cmd.append(input_file)
-                process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+        cmd = ['python', os.path.join(self.prog_dir, f'{program}.py')] + args
+
+        # Run the program with the provided arguments
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Check if the process exited with a non-zero status
         self.assertEqual(process.returncode, 0, f"Program exited with {process.returncode}. Error: {process.stderr.decode('utf-8')}")
-    
+
         # Get the actual output from the program
         actual_output = process.stdout.decode('utf-8').strip()
-    
+
         # Compare the actual output to the expected output
         with open(expected_file, 'r') as f:
             expected_output = f.read().strip()
-    
+        
         self.assertEqual(actual_output, expected_output, f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
-
 
     def test_programs(self):
         for filename in os.listdir(self.test_dir):
             if filename.endswith('.in'):
                 program, test_name = filename[:-3].split('.', 1)
-                self.run_test(program, test_name, use_args=False)
-                arg_file = f'{program}.{test_name}.arg.out'
-                if arg_file in os.listdir(self.test_dir):
-                    self.run_test(program, test_name, use_args=True)
+                self.run_test(program, test_name, use_args=True) # Set to True to use command line arguments
 
 if __name__ == '__main__':
     unittest.main()
