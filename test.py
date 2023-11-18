d0f0efb77b1d8352cf0bef6b3d71e57ea89e794f
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-11-18 16:43:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,20 +6,30 @@ class TestProgram(unittest.TestCase):
     test_dir = './test'
     prog_dir = './prog'
 
-    def run_test(self, program, test_name, use_args=False):
+    def run_test(self, program, test_name, use_args):
         input_file = f'{self.test_dir}/{program}.{test_name}.in'
         expected_file = f'{self.test_dir}/{program}.{test_name}'+ ('.arg.out' if use_args else '.out')
 
-        # Read arguments from the .in file
-        with open(input_file, 'r') as f:
-            # Split the arguments into a list. Assumes that the input file has the arguments on one line.
-            args = f.read().strip().split(' ', 1)
+        # Construct the command with the path to the Python script
+        cmd = ['python3', os.path.join(self.prog_dir, f'{program}.py')]
 
-        # Construct the command
-        cmd = ['python', os.path.join(self.prog_dir, f'{program}.py')] + args
-
-        # Run the program with the provided arguments
-        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Handle program-specific execution
+        if program == 'gron':
+            # If program is gron, expect JSON input from a file or STDIN
+            if use_args:
+                # Pass the filename as an argument
+                cmd.append(input_file)
+            else:
+                # Pass JSON data through STDIN
+                with open(input_file, 'r') as f:
+                    input_content = f.read()
+                process = subprocess.run(cmd, input=input_content.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            # For file_finder and wc, read the .in file and pass arguments
+            with open(input_file, 'r') as f:
+                args = f.read().strip().split(' ', 1)
+            cmd.extend(args)
+            process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         # Check if the process exited with a non-zero status
         self.assertEqual(process.returncode, 0, f"Program exited with {process.returncode}. Error: {process.stderr.decode('utf-8')}")
@@ -37,7 +47,9 @@ class TestProgram(unittest.TestCase):
         for filename in os.listdir(self.test_dir):
             if filename.endswith('.in'):
                 program, test_name = filename[:-3].split('.', 1)
-                self.run_test(program, test_name, use_args=True) # Set to True to use command line arguments
+                # Determine if command line arguments should be used based on the program
+                use_args = True if program == 'file_finder' else False
+                self.run_test(program, test_name, use_args)
 
 if __name__ == '__main__':
     unittest.main()
