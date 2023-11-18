f7004ec2ff445bea7e59d64832e98e6c2d491a4f
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-11-18 14:05:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,37 +8,34 @@ class TestProgram(unittest.TestCase):
 
     def run_test(self, program, test_name, use_args=False):
         input_file = f'{self.test_dir}/{program}.{test_name}.in'
-        expected_file = f'{self.test_dir}/{program}.{test_name}'+ ('.arg.out' if use_args else '.out')
+        expected_file = f'{self.test_dir}/{program}.{test_name}' + ('.arg.out' if use_args else '.out')
     
-        # Correctly append the '.py' to the program name
+        # Prepare the command with the full path to the Python script
         cmd = ['python3', os.path.join(self.prog_dir, f'{program}.py')]
     
-        # The following block needs to handle use_args correctly
+        # If use_args is True, the arguments are read from the .in file and passed to the command
         if use_args:
-            # Read arguments from the .in file
             with open(input_file, 'r') as f:
                 args = f.read().strip().split()
-            # Pass the arguments to the program
-            cmd.extend(args)
+            cmd.extend(args)  # Add the arguments from the .in file to the command
             process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
         else:
-            # If use_args is False, pass input_file as STDIN
+            # If use_args is False, the content of the .in file is passed as STDIN to the script
             with open(input_file, 'rb') as f:
                 process = subprocess.run(cmd, stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
-        # Check if the process exited with a non-zero status
+        # Check if the process exited with a non-zero status and output any errors
         self.assertEqual(process.returncode, 0, f"Program exited with {process.returncode}. Error: {process.stderr.decode('utf-8')}")
     
         # Get the actual output from the program
-        actual_output = process.stdout.decode('utf-8')
+        actual_output = process.stdout.decode('utf-8').strip()
     
         # Compare the actual output to the expected output
         with open(expected_file, 'r') as f:
             expected_output = f.read().strip()
         
-        self.assertEqual(actual_output.strip(), expected_output, 
-                         f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
+        # Assert that the actual output matches the expected output
+        self.assertEqual(actual_output, expected_output, f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
 
     def test_programs(self):
         for filename in os.listdir(self.test_dir):
