74f721c48b2999971d5632b8b3ba11c4499f4505
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-11-17 21:02:38 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,41 +2,45 @@ import subprocess
 import os
 import unittest
 
-class TestHarness(unittest.TestCase):
+class TestProgram(unittest.TestCase):
+    test_dir = './test'
+    prog_dir = './prog'
 
-    def run_test_case(self, program, input_file, expected_output_file, arg=False):
-        # If arg is True, pass input_file as a command-line argument, else use STDIN
-        if arg:
-            proc = subprocess.run([f'./{program}', input_file], capture_output=True, text=True)
+    def run_test(self, program, test_name, use_args=False):
+        input_file = f'{self.test_dir}/{program}.{test_name}.in'
+        expected_file = f'{self.test_dir}/{program}.{test_name}'+ ('.arg.out' if use_args else '.out')
+
+        # If use_args is True, the input should be passed as a command-line argument
+        cmd = [os.path.join(self.prog_dir, program)]
+        if use_args:
+            cmd.append(input_file)
         else:
-            with open(input_file, 'r') as f:
-                proc = subprocess.run([f'./{program}'], stdin=f, capture_output=True, text=True)
+            with open(input_file, 'rb') as f:
+                process = subprocess.run(cmd, stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                self.assertEqual(process.returncode, 0)
+                actual_output = process.stdout.decode('utf-8')
+
+        # When use_args is True, we need to execute the program again
+        # as some programs might behave differently when using arguments vs. stdin
+        if use_args:
+            process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            self.assertEqual(process.returncode, 0)
+            actual_output = process.stdout.decode('utf-8')
+
+        with open(expected_file, 'r') as f:
+            expected_output = f.read().strip()
         
-        # Check if the process exited with a non-zero status
-        self.assertEqual(proc.returncode, 0)
-        
-        # Compare the actual output to the expected output
-        with open(expected_output_file, 'r') as f:
-            expected_output = f.read()
-        self.assertEqual(proc.stdout, expected_output)
+        self.assertEqual(actual_output.strip(), expected_output, 
+                         f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
 
     def test_programs(self):
-        test_dir = './test/'
-        for filename in os.listdir(test_dir):
+        for filename in os.listdir(self.test_dir):
             if filename.endswith('.in'):
-                basename = filename[:-3]  # Strip off '.in' to get the base filename
-                program, test_case = basename.split('.', 1)
-
-                # Test using STDIN
-                input_file = os.path.join(test_dir, f'{program}.{test_case}.in')
-                expected_output_file = os.path.join(test_dir, f'{program}.{test_case}.out')
-                if os.path.exists(expected_output_file):
-                    self.run_test_case(program, input_file, expected_output_file)
-
-                # Test using command-line argument
-                arg_output_file = os.path.join(test_dir, f'{program}.{test_case}.arg.out')
-                if os.path.exists(arg_output_file):
-                    self.run_test_case(program, input_file, arg_output_file, arg=True)
+                program, test_name = filename[:-3].split('.', 1)
+                self.run_test(program, test_name, use_args=False)
+                arg_file = f'{program}.{test_name}.arg.out'
+                if arg_file in os.listdir(self.test_dir):
+                    self.run_test(program, test_name, use_args=True)
 
 if __name__ == '__main__':
     unittest.main()
