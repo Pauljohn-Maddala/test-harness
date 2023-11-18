9a6c83eabb062526c1408fcf57a1cc7c28e97b15
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-11-18 15:22:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,42 +7,28 @@ class TestProgram(unittest.TestCase):
     prog_dir = './prog'
 
     def run_test(self, program, test_name, use_args=False):
-        input_file = f'{self.test_dir}/{program}.{test_name}.in'
-        expected_file = f'{self.test_dir}/{program}.{test_name}' + ('.arg.out' if use_args else '.out')
-    
-        # Construct the command with the path to the Python script
-        cmd = ['python3', os.path.join(self.prog_dir, f'{program}.py')]
-    
-        # Read arguments from the .in file
-        with open(input_file, 'r') as f:
-            # Assumes arguments are separated by newlines or spaces
-            args = f.read().strip().split()
+    input_file = f'{self.test_dir}/{program}.{test_name}.in'
+    expected_file = f'{self.test_dir}/{program}.{test_name}' + ('.arg.out' if use_args else '.out')
 
-        # Inside the run_test method, before running the wc.py script
-        if program == 'wc':
-            print('Running wc test with the following input:')
-            with open(input_file, 'r') as f:
-                print(f.read())
-            #print('Expected output:', expected_output)
-        
-        # Pass the arguments to the program
-        if use_args:
-            cmd.extend(args)
-            process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        else:
-            process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=args[0].encode())
-    
-        # Check if the process exited with a non-zero status
-        self.assertEqual(process.returncode, 0, f"Program exited with {process.returncode}. Error: {process.stderr.decode('utf-8')}")
-    
-        # Get the actual output from the program
-        actual_output = process.stdout.decode('utf-8').strip()
-    
-        # Compare the actual output to the expected output
-        with open(expected_file, 'r') as f:
-            expected_output = f.read().strip()
-        
-        self.assertEqual(actual_output, expected_output, f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
+    cmd = ['python3', os.path.join(self.prog_dir, f'{program}.py')]
+
+    if use_args or program == 'wc':
+        # If use_args is True or program is wc, pass input_file as a command-line argument
+        cmd.append(input_file)
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        # Else, pass input_file as STDIN
+        with open(input_file, 'rb') as f:
+            process = subprocess.run(cmd, stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    self.assertEqual(process.returncode, 0, f"Program exited with {process.returncode}. Error: {process.stderr.decode('utf-8')}")
+
+    actual_output = process.stdout.decode('utf-8').strip()
+
+    with open(expected_file, 'r') as f:
+        expected_output = f.read().strip()
+
+    self.assertEqual(actual_output, expected_output, f'Failed test: {program}.{test_name} with {"arguments" if use_args else "stdin"}')
 
         
 
