a27d2c61e4713c930c4ff45e22c8c43f6ab3a162
Pauljohn-Maddala <60296709+Pauljohn-Maddala@users.noreply.github.com>
2023-11-18 14:31:24 -0500

    Update file_finder.py

--- prog/file_finder.py
+++ prog/file_finder.py
@@ -28,10 +28,15 @@ def file_finder(root_dir, pattern='*', min_size=None, max_size=None, before=None
 
 def main():
     parser = argparse.ArgumentParser(description='Search for files based on various criteria.')
-    parser.add_argument('./', help='Root directory to search for files.')
+    parser.add_argument('./test', help='Root directory to search for files.')
+    parser.add_argument('--pattern', help='Pattern to match filenames, e.g., "*.txt".', default='*')
     # Add other parser arguments as needed
-    args = parser.parse_args()
-    matches = file_finder(args.root_dir) # and other arguments
+     args = parser.parse_args()
+    matches = file_finder(
+        args.root_dir,
+        pattern=args.pattern
+    )
+    #matches = file_finder(args.root_dir) # and other arguments
     for match in matches:
         print(match)
 
